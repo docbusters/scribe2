a68fdf618a5c892cde7d5afc3caae4591e7c29cf
Antonio Martínez <antoniom220502@gmail.com>
2026-06-02 15:08:18 +0200

    Fixed map marker displaying small

--- src/lib/components/utilComponents/MapLibreMap.svelte
+++ src/lib/components/utilComponents/MapLibreMap.svelte
@@ -257,8 +257,8 @@
 	.marker-content {
 		border-radius: 100%;
 		padding: 0.5rem;
-        width: 1.75rem;
-        height: 1.75rem;
+        min-width: 1.75rem;
+        min-height: 1.75rem;
 		background-color: var(--scribe-doc-background);
 		box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
 		border: 1px solid var(--scribe-border-color);
